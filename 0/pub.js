85273369d55583952c81269a43cc1691ccf3f760
tlimpt <43952302+tlimpt@users.noreply.github.com>
2022-02-24 14:14:03 +0000

    Add files via upload

---
+++ 0/pub.js
@@ -0,0 +1,14 @@
+const pub = 1;
+const des = "https://disney.com/";
+const img = "/pub.jpg";
+const tex = "Miau! Este é um texto promocional deste <a id='pub'>produto maravilhoso</a> que deve comprar. Pode conter legalmente até 120 caracteres!";
+
+/** Não mexer. **/
+if ( pub != 0 ) {
+	const h = document.createElement("header");
+	h.innerHTML = "<span><small>PUB.</small></span><span><a id='pub1'><img id='pub2' alt=''></a></span><span>" + tex + "<span>";
+	document.body.insertBefore(h, document.body.firstChild);
+	document.getElementById("pub").href= des;
+	document.getElementById("pub1").href= des;
+	document.getElementById("pub2").src= img;
+}

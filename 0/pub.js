135229651838ce751d03d1d52c94c2e1592e315b
tlimpt <43952302+tlimpt@users.noreply.github.com>
2022-02-27 14:40:39 +0000

    Add files via upload

--- 0/pub.js
+++ 0/pub.js
@@ -6,7 +6,7 @@ const tex = "Miau! Este é um texto promocional deste <a id='pub'>produto maravi
 /** Não mexer. **/
 if ( pub != 0 ) {
 	const h = document.createElement("header");
-	h.innerHTML = "<span><small>PUB.</small></span><span><a id='pub1'><img id='pub2' alt=''></a></span><span>" + tex + "<span>";
+	h.innerHTML = "<span><small>PUB.</small></span><span><a id='pub1'><img id='pub2' alt='Publicidade'></a></span><span>" + tex + "<span>";
 	document.body.insertBefore(h, document.body.firstChild);
 	document.getElementById("pub").href= des;
 	document.getElementById("pub1").href= des;

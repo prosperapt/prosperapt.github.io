fa43adc3bd8cfa61fdc68aca6b72f84b89b25b64
tlimpt <43952302+tlimpt@users.noreply.github.com>
2022-02-28 09:48:16 +0000

    Update processar.js

--- anunciar/processar.js
+++ anunciar/processar.js
@@ -3,10 +3,18 @@ import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.14/mod.ts';
 // Step 1: Read the downloaded_filename JSON
 const filename = Deno.args[0]; // Same name as downloaded_filename `const filename = 'btc-price.json';`
 const json = await readJSON(filename);
+console.log(json);
 
 // Step 2: Filter specific data we want to keep and write to a new JSON file
 const v = Object.values(json)[7];
+console.log(v);
 
 // Step 3. Write a new JSON file with our filtered data
 const newFilename = 'anunciar/conta.json'; // name of a new file to be saved
-await writeJSON(newFilename, v, { append: true }); // create a new JSON file with just the Bitcoin price
+// await writeJSON(newFilename, v, { append: true }); // create a new JSON file with just the Bitcoin price
+
+const anterior = await readTextFile(newFilename);
+console.log(anterior);
+const proximo = anterior + "\n" + v;
+console.log(proximo);
+await writeTextFile(newFilename, proximo);
